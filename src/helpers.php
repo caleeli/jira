ed7be3b780066ba5143c979355815d346c0670ea
David Callizaya <davidcallizaya@gmail.com>
2024-06-19 18:45:32 -0400

    Add helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,10 +2,39 @@
 
 use Codeflow\Jira\JiraService;
 
-function getTicketsAssignedToUser($username)
+/**
+ * Get tickets assigned to a user
+ *
+ * @param mixed $username The username of the user
+ * @return array The tickets assigned to the user
+ */
+function getTicketsAssignedToUser($username): array
 {
     $jira = new JiraService();
-    $tickets = $jira->getTicketsAssignedToUser($username);
-    
-    return $tickets;
+    return $jira->getTicketsAssignedToUser($username);
 }
+
+function import_jira_function()
+{
+    $definitions = [];
+    // get functions declared in this file
+    $functions = get_defined_functions();
+    $functions = $functions['user'];
+    $functions = array_filter($functions, function ($function) {
+        return strpos($function, 'import_') !== 0;
+    });
+    foreach ($functions as $function) {
+        $reflection = new ReflectionFunction($function);
+        $filename = $reflection->getFileName();
+        $docblock = $reflection->getDocComment();
+        if ($filename === __FILE__) {
+            $line = $reflection->getStartLine();
+            $code = trim(file($filename)[$line - 1], ' {');
+            $definitions[] = $docblock;
+            $definitions[] = $code;
+        }
+    }
+    return implode("\n", $definitions);
+}
+
+echo import_jira_function();
